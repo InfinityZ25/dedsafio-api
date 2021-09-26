dd8a52cad17b9daaf6eba44d94745c4492ddd874
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-09-26 00:59:50 -0400

    added currentSet

--- src/routes/players.js
+++ src/routes/players.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const index = require("../index");
 const axios = require("axios").default;
 
+var currentSet = "ffa";
+
 // List all the players in their respective team
 router.get("", async (req, res) => {
   index.getRedisClient().hgetall("ffa", async (err, reply) => {
     if (err) throw err;
     var response = await real(reply);
     //JSON.parse(reply);
-    res.json({ response });
+    res.json({ dataset: currentSet, response });
   });
 });
 // List a specific player and their team
