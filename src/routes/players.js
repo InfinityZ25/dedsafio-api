29432a576069e5cbf6a385359de7a5d2352f8fa3
Juan Cedeno <37850007+InfinityZ25@users.noreply.github.com>
2021-09-26 15:01:08 -0400

    Added local cache to improve speeds

--- src/routes/players.js
+++ src/routes/players.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const index = require("../index");
+const NodeCache = require("node-cache");
 const axios = require("axios").default;
 
 // Current set is the name of the hash in the redis backend.
 var currentSet = "ffa";
 // The name of the hash to store the cached playerdb.co responses in.
 let cachedNamesSetName = process.env.CACHED_SET || "uuids:names";
+// Cache map to improve speeds locally
+const nCache = new NodeCache({ stdTTL: 120 });
 
 // List all the players in their respective team
 router.get("", async (req, res) => {
@@ -28,14 +31,15 @@ async function real(reply) {
   for (const key of keys) {
     var content = JSON.parse(reply[key]);
     var modifiedContent = await modifyObjectToIncludeNames(content);
-    //console.log(modifiedContent);
     response.push(modifiedContent);
   }
   return response;
 }
 
 async function modifyObjectToIncludeNames(teamObject) {
+  // Obtain the players from the team object's members array.
   var players = teamObject.members;
+  // Initialize a new array to stored the player objects with real names.
   var playersWithNames = [];
 
   for (const player in players) {
@@ -46,57 +50,89 @@ async function modifyObjectToIncludeNames(teamObject) {
     }
     playersWithNames.push(playerObject);
   }
+  // Add the playersWithNams array to the teamObject.
   teamObject.players = playersWithNames;
+  // Delete the default teamObject members array from the copy that will be sent to the client.
   delete teamObject.members;
+  // Return the teamObject.
   return teamObject;
 }
 
+/**
+ * A function that queries the redis backend hash for the real name of a uuid. If not found, it queries the playerdb.co API.
+ *
+ * @param {*} id The uuid of the player. Stringified UUID.
+ * @returns {Promise<string>} A promise that resolves to the real name of the player or null if not existing.
+ */
+async function getPlayerName(id) {
+  // Check local cache first
+  if (nCache.has(id)) {
+    console.log("pulling from local cache");
+    var playerName = nCache.get(id);
+    console.log(playerName);
+    return playerName;
+  }
+  // Ask redis for the name of the player, if cached.
+  var name = await index.getRedisClient().hget(cachedNamesSetName, id);
+  // If found, return the name.
+  if (name != null) {
+    // Parse to Json to ensure everything is working fine.
+    var responseObject = JSON.parse(name);
+    // Write into local cache for 120 seconds.
+    nCache.set(id, responseObject.name, 120);
+    // Return object field name if it exists. Otherwise return null.
+    return responseObject.name;
+  } else {
+    // Otherwise, ask playerdb.co for the name.
+    var response = await getPlayerNameFromPlayerdb(id);
+    // Write into local cache for 120 seconds.
+    nCache.set(id, response, 120);
+    return response;
+  }
+}
+/**
+ * A function that queries the playerdb.co API for the real name of a uuid.
+ * This function also caches the response in the redis backend hash.
+ *
+ * @param {*} id The uuid of the player. Stringified UUID.
+ * @returns {Promise<string>} A promise that resolves to the real name of the player.
+ */
+async function getPlayerNameFromPlayerdb(id) {
+  try {
+    // Ask playerdb for the player nname using a uuid.
+    const response = await axios.get(
+      `https://playerdb.co/api/player/minecraft/${id}`
+    );
+    // Obtain the object data from the response.
+    var playerQuery = response.data;
+    // Use the code to determine if the player does exist or not.
+    if (playerQuery.code === "player.found") {
+      // Obtain the username from the response
+      var playerName = playerQuery.data.player.username;
+      // Write without blocking the name to the redis backend hash and return the name.
+      writeNameForId(id, playerName);
+      return playerName;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  // If we get here, the player doesn't exist. Write them as null and return null.
+  writeNameForId(id, null);
+  return null;
+}
+
+/**
+ * A function that writes data regarding the real player name of a uuid to the redis backend hash.
+ * @param {*} id The uuid of the player. Stringified UUID.
+ * @param {*} name The real name of the player. If null or -1 is passed, the player is assumed to not exist.
+ * @returns {Promise<number>} A promise that resolves when the data is written to the redis backend.
+ */
 async function writeNameForId(id, name) {
-  let timeStamp = new Date().getTime();
-  index
+  let timeStamp = new Date().getTime(); // Timestamp to know when the data was last updated.
+  return index
     .getRedisClient()
     .hset(cachedNamesSetName, id, JSON.stringify({ name, timeStamp }));
 }
 
-async function getPlayerName(id) {
-  var name = await index.getRedisClient().hget(cachedNamesSetName, id);
-
-  if (name != null) {
-    var responseObject = JSON.parse(name);
-    responseObject.id = id;
-    //console.log(responseObject);
-    return responseObject.name;
-  } else {
-    console.log("Null querying db");
-    var response = await getPlayerNameFromPlayerdb(id);
-    return response || -1;
-  }
-}
-
-async function getPlayerNameFromPlayerdb(id) {
-  try {
-    //Ask playerdb for the player name
-    const response = await axios.get(
-      `https://playerdb.co/api/player/minecraft/${id}`
-    );
-    console.log(response);
-    var player = response.data;
-
-    if (player.code === "player.found") {
-      var playerName = player.data.player.username;
-      writeNameForId(id, playerName);
-      console.log("player found");
-      return playerName;
-    } else {
-      //If the playerdb api fails, mojang doesn't know the player, and thus they don't exist.
-      console.log("player not found");
-      writeNameForId(id, -1);
-      return "null";
-    }
-  } catch (error) {
-    console.log("player not found");
-    writeNameForId(id, null);
-  }
-}
-
+// Exports the express router for index.js to use.
 module.exports = router;
